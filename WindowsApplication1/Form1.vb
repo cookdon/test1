78c5950121a5045afede1ced91f7ca84bba7879a
cookdon <kengo_ito@toasystemkk.co.jp>
2015-01-28 17:35:09 +0900

    proc2

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -3,4 +3,8 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("Pushed " & Button2.Name & "!!")
+    End Sub
 End Class
